0f77690c8367c3ec41b1db86f9fa0b6514c5e6cb
torvic886 <vagutierrezv@uqvirtual.edu.co>
2024-06-11 18:09:41 -0500

    build(fundamentos javascript): introduccion
    Elementos Basicos JavaScript y Consola

---
+++ 01 - fundamentos/app.js
@@ -0,0 +1,23 @@
+//console.log('hola mundo');
+
+let a = 50,
+    b = 10,
+    c = 'Hola',
+    d = 'Spiderman',
+    x = a + b ;
+
+//  console.log( x );   
+//  console.error( x );   
+//  console.warn( x );   
+
+ console.log( 'a: ', a );   
+ console.log( 'b: ', b );   
+ console.log( 'c: ', c );  
+
+console.log('%c Mis Variables', 'color: red; font-weight: bold');
+
+ console.log( { a } );   // objeto
+ console.log( { b } );   
+ console.log( { c } );  
+
+ console.table({a,b,c,d,x})
